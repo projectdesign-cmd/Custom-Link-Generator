c26bf9cef1a773693d337ff7a94888896b1d8c9c
projectdesign-cmd <xawog94562@gmail.com>
2026-08-14 13:55:03 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,16 +1,11 @@
 export default function handler(req, res) {
   const { id } = req.query;
 
-  const redirects = {
-    "g76wq0uk8":
-      "https://www.effectivecpmnetwork.com/g76wq0uk8?key=7692e6337b3c7e4a5882748459c3129a"
-  };
-
-  const destination = redirects[id];
-
-  if (!destination) {
-    return res.status(404).send("Link not found");
+  if (!id) {
+    return res.status(400).send("Missing file ID");
   }
 
-  return res.redirect(302, destination);
+  const targetUrl = `https://urlking.in/file_${encodeURIComponent(id)}`;
+
+  res.redirect(302, targetUrl);
 }
